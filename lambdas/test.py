deb96522bfae3223e3e0656880008f55b215bb2c
giancarlolottero-cell <giancarlo.lottero@dataiq.com.ar>
2026-03-17 16:46:24 -0300

    Add lambda_handler function to return greeting
    Implement basic lambda function that returns a greeting.

--- lambdas/test.py
+++ lambdas/test.py
@@ -1,1 +1,8 @@
-# Esto es una lambda
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
